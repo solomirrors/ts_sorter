e2126c59dadcc08ae4288319b49b131e8ef28705
solomirrors <solomirrors@icloud.com>
2021-12-21 21:12:49 +0100

    Separation of comparison and exchange

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Sorter from "./Sorter";
+import NumbersCollection from "./NumbersCollection";
 
-class Sorter {
-    constructor(public collection: number[]) {}
-    sort(): void {
-        const { length } = this.collection;
+const numbersCollection =
+    new NumbersCollection([11, -3, -11, 3, 1, -1, -2, 2]);
+const sorter =
+    new Sorter(numbersCollection)
 
-        for (let i = 0; i < length; i++){
-            for (let j = 0; j < length - i - 1; j++){
-                if (this.collection[j] > this.collection[j+1]){
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j+1];
-                    this.collection[j+1] = leftHand;
-                }
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([11, -3, -11, 3, 1, -1, -2, 2])
 sorter.sort();
 console.log(sorter.collection)
 
